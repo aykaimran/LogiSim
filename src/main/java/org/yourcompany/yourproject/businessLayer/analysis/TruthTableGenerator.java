4911e5818e32ea482b9a501f8317c0569e2a15b0
Tayyaba Fatima <158576100+tayyabafatima08@users.noreply.github.com>
2025-11-17 04:37:21 +0500

    [Added Truth Table Generator file in Analysis Folder]

---
+++ src/main/java/org/yourcompany/yourproject/businessLayer/analysis/TruthTableGenerator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.yourcompany.yourproject.businessLayer.analysis;
+
+/**
+ *
+ * @author Tabs
+ */
+public class TruthTableGenerator {
+    public TruthTableGenerator() {
+        // Logic to generate truth table will go here
+    }
+}
